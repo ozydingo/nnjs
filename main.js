ffe482d03ba2cfce6a9eeb45bbe4ef7fc10996a9
Andrew Schwartz <andrew@Andrews-MacBook-Pro.local>
2018-04-12 07:22:09 -0400

    feat(main.js) Paint network on doc ready

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
   var network = new nnjs.Network([2,4,1]);
   var svg = $("#netsvg")[0];
   runner = new nnjs.Runner(network, svg);
+  runner.paint();
 })
 
 $(document).on("click", "#play", function() {
