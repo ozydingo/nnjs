1c145e84855868f47f517cbb1891cbfc3de61473
Andrew Schwartz <andrew@Andrews-MacBook-Pro.local>
2018-04-18 11:04:52 -0400

    refactor(NetworkUI) [AS] Pull button listeners from mian.js into nnjs.NetworkUI.js

--- main.js
+++ main.js
@@ -5,49 +5,8 @@ $(document).ready(function(){
   var network = new nnjs.Network([2,4,1]);
   var netsvg = $("#netsvg")[0];
   var outcvs = $("#output_canvas")[0];
+  var controls = $("#main-controls");
   runner = new nnjs.Runner(network, netsvg, outcvs);
-  netui = new nnjs.NetworkUI(runner);
+  netui = new nnjs.NetworkUI(runner, controls);
   runner.paint();
 })
-
-$(document).on("click", "#play", function() {
-  $("#play").hide();
-  $("#pause").show();
-  runner.run();
-});
-
-$(document).on("click", "#pause", function() {
-  $("#pause").hide();
-  $("#play").show();
-  runner.pause();
-});
-
-$(document).on("click", "#dec-layers", function() {
-  var hacker = new nnjs.NetworkHacker(runner.network);
-  hacker.remove_layer();
-  runner.paint();
-});
-
-$(document).on("click", "#inc-layers", function() {
-  var hacker = new nnjs.NetworkHacker(runner.network);
-  hacker.add_layer(3);
-  runner.paint();
-});
-
-$(document).on("click", "#dec-neurons", function() {
-  var layer = runner.painter.selected_layer;
-  if(layer === null) {return;}
-
-  var hacker = new nnjs.NetworkHacker(runner.network);
-  hacker.remove_neuron(layer);
-  runner.paint();
-});
-
-$(document).on("click", "#inc-neurons", function() {
-  var layer = runner.painter.selected_layer;
-  if(layer === null) {return;}
-
-  var hacker = new nnjs.NetworkHacker(runner.network);
-  hacker.add_neuron(layer);
-  runner.paint();
-});
